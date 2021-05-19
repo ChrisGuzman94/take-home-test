5b63fb77ef297f82afeb5f4f8a260fe72af57837
ChrisGuzman94 <39037648+ChrisGuzman94@users.noreply.github.com>
2021-05-19 10:31:35 -0400

    Add files via upload

---
+++ jdoodle.js
@@ -0,0 +1,33 @@
+ 
+f = (value) => 
+{
+  
+    switch(value) {
+        
+    case A:
+      z();
+      break;
+    
+    case B:
+       y();
+       break;
+    
+    case C:
+       x();
+       break;
+    
+    case D:
+       w();
+       return true
+    
+    default:
+      v();
+      u();
+      t();
+      s();
+      return false
+    }    
+}
+
+
+
